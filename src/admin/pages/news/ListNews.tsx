09e9d4faf3dbdb94adb78025ad7e9c76e953b366
terra456 <terra.456@yandex.ru>
2023-09-18 14:06:06 +0500

    fix: assets ways

--- src/admin/pages/news/ListNews.tsx
+++ src/admin/pages/news/ListNews.tsx
@@ -7,7 +7,7 @@ const ListNews = () => {
       <h1 className="admin-title admin-news__title">Новости</h1>
       <div className="admin-news__content">
         <Link to={'add'} className="admin-add-card admin-add-card_type_news">
-          <img src="../assets/images/icons/add.svg" alt="Добавить запись" className="admin-add-card__icon" />
+          <img src="./assets/images/icons/add.svg" alt="Добавить запись" className="admin-add-card__icon" />
           <p className="admin-add-card__text">Добавить новую запись</p>
         </Link>
         <AdminNewsCard />
