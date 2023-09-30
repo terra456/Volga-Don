c559a86738b246df37795c2fdbbbf5bbc1cb64d7
terra456 <terra.456@yandex.ru>
2023-09-30 23:09:16 +0500

    fix: gitignore and vite config for build

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,20 @@ import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 const root = resolve(__dirname, 'src');
-// const outDir = resolve(__dirname, 'dist');
+const outDir = resolve(__dirname, 'dist');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root,
   plugins: [react()],
-  // build: {
-  //   outDir,
-  //   emptyOutDir: true,
-  //   rollupOptions: {
-  //     input: {
-  //       main: resolve(root, 'index.html'),
-  //       admin: resolve(root, 'admin', 'index.html'),
-  //     },
-  //   },
-  // },
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        // admin: resolve(root, 'admin', 'index.html'),
+      },
+    },
+  },
 });
